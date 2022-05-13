b681f317f2329aa51111243b98c0494c84e46466
silv_etooo <63814405+Silv-etooo@users.noreply.github.com>
2022-05-13 18:10:37 +0300

    Update dynamicArray.h

--- Second Year/Summer/Oop/Proekt1/dynamicArray.h
+++ Second Year/Summer/Oop/Proekt1/dynamicArray.h
@@ -9,12 +9,11 @@ public:
         capacity = 8;
         text = new char[capacity];
         text[0] = '\0';
-        //text[1] = '\0';
         index = 0;
     } 
 
     ~dynamicArray(){
-        delete text;
+        delete[] text;
     }
 
     void inputArray(){
@@ -24,7 +23,7 @@ public:
         text[0] = ch;
         while(ch != '\n'){
             index++;
-            if(index > capacity - 1) resize(text, capacity);
+            if(index >= capacity - 1) resize(text, capacity);
 
             cin.get(ch);
 
@@ -32,16 +31,13 @@ public:
         }
         text[index] = '\0';
 
-
     }
    
     void inputCharacter(char ch){
+        if(index >= capacity - 1) resize(text, capacity);
 
-        if(index > capacity - 1) resize(text, capacity);
-
-        text[index] = ch;
-        text[index+1] = '\0';
-        index++;
+        text[index++] = ch;
+        text[index] = '\0';
     }
 
     void inputString(const char* niz){
@@ -55,7 +51,7 @@ public:
 
     void inputNumber(int num){
 
-        char temp[4] = {'\0', '\0', '\0','\0'};
+        char temp[4] = {'`', '`', '`','`'};
 
         int i = 3;
 
@@ -67,7 +63,7 @@ public:
        
         i = 0;
         while(i < 4 ){
-            if(temp[i] != '\0'){
+            if(temp[i] != '`'){
                 this->inputCharacter(temp[i]);
             }
         i++;
@@ -84,41 +80,70 @@ public:
     }
 
     void clear(){
-        delete text;
-        text = new char[capacity];
-        text[0] = '\0';
+        delete[] text;
         capacity = 8;
         index = 0;
+        text = new char[capacity];
+        text[0] = '\0';
+        
     }
 
 
     const char* getText() const { return text; }
-    
+    int getCapacity() const { return capacity; }
+    int getIndex() const { return index; }
 
 
+
+    dynamicArray& operator=(const dynamicArray& other){
+        if(this != &other){
+            delete[] text;
+
+            capacity = other.getCapacity();
+            index = other.getIndex();
+            text = new char[capacity];
+            int i = 0;
+            while((other.getText())[i]){
+                text[i] = (other.getText())[i];
+                i++;
+            }
+            text[i] = '\0';
+        }
+        return *this;
+    }
     
 private:
     char *text;
     int capacity;
     int index;
 
-    void resize(char *&text, int& capacity){
-        //1 
-        char * newText = new char[capacity*2];
-        if(!newText) cout << "error" <<endl;
+    // void resize(char *&text, int& capacity){
+    //     //1 
+    //     char * newText = new char[capacity*2];
+    //     if(!newText) cout << "error" <<endl;
 
-        //2
-        int i = 0;
-        while(text[i] != '\0'){
-            newText[i] = text[i];
-            i++;
-        } 
-        //3
-        capacity *= 2;
-        //4
-        delete[] text;
-        //5
-        text = newText;
+    //     //2
+    //     int i = 0;
+    //     while(text[i] != '\0'){
+    //         newText[i] = text[i];
+    //         i++;
+    //     } 
+    //     //3
+    //     capacity *= 2;
+    //     //4
+    //     delete[] text;
+    //     //5
+    //     text = newText;
+    // }
+    void resize(char* &str, int& size) {
+        char* oldStr = str;
+        str = new char[size*2];
+
+        for (int i = 0; i < size; i++) {
+            str[i] = oldStr[i];
+        }
+        size *= 2;
+        delete[] oldStr;
     }
 }; 
 
