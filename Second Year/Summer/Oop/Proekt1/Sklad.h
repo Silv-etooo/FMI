1c8404b1f442451ff136c7a96071dc6d428791d8
silv_etooo <63814405+Silv-etooo@users.noreply.github.com>
2022-05-13 18:10:21 +0300

    Update Sklad.h

--- Second Year/Summer/Oop/Proekt1/Sklad.h
+++ Second Year/Summer/Oop/Proekt1/Sklad.h
@@ -5,11 +5,11 @@ class Sklad{
 public:
     Sklad();
 
-    void print(); // Извеждане
-    void add(); // Добавяне на продукт
+    void print(); // Извеждане //.
+    void add(); // Добавяне на продукт //.
     void remove(); // Изваждане на продукт
-    void inquiry(); // Справка за наличност
-    void clearSklad(); // Разчистване 
+    void inquiry(); // Справка за наличност //
+    void clearSklad(); // Разчистване //...
     
 
 private:
@@ -17,7 +17,7 @@ private:
     Product* products;
     int num;
 
-    int taken_Locations[9][9][9]; //10 на брой с пореден номер на рафт  //10 на брой с рафта //10 секции
+    bool taken_Locations[9][9][9]; //10 секции //10 на брой с рафта  //10 на брой с пореден номер на рафт  
     //за локацията е дали е заето 
     
 
@@ -25,4 +25,5 @@ private:
     //nali4nost file
     void readProductFromFile();
     void writeProductToFile(const Product & product);
+    void removeProduct(int index);
 };
